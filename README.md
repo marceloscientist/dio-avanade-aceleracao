af7412a6bf5fb2ca924380553c4bae44f50abd21
Marcelo Carvalho <marcelo.scientist@hotmail.com>
2020-10-25 23:47:18 -0300

    Directed index to post straight away

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyCrudApp
+# AvanadeAceleracao
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.3.
 
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -6,6 +6,7 @@ import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
   
 const routes: Routes = [
+  { path: '', redirectTo: 'post/index', pathMatch: 'full'},
   { path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
   { path: 'post/index', component: IndexComponent },
   { path: 'post/:postId/view', component: ViewComponent },
